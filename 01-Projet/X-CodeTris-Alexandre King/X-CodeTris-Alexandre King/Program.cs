7bf21b314e5026ffdbd13c6a2b1b96e65bde59ab
Xale2111 <pd50uyl@eduvaud.ch>
2024-04-29 11:02:05 +0200

    Push origin with all documentation and empty project

---
+++ 01-Projet/X-CodeTris-Alexandre King/X-CodeTris-Alexandre King/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_CodeTris_Alexandre_King
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
